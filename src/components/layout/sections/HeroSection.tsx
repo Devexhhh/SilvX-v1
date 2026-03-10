c42bd63e3fcee5a64d82d3cdcfb337330133eb5e
Devexhhh <kumarsujal.codes@gmail.com>
2026-03-10 19:21:09 +0530

    fixed HeroSection

--- src/components/layout/sections/HeroSection.tsx
+++ src/components/layout/sections/HeroSection.tsx
@@ -3,20 +3,20 @@ import VerticalLine from "../../ui/VerticalLine";
 
 export default function HeroSection() {
     return (
-        <section className="flex flex-col xl:flex-row relative px-12 py-24 gap-12">
-            <div className="flex-1 flex flex-col justify-center space-y-12">
-                <h1 className="text-5xl md:text-[50px] font-normal leading-tight">
-                    The Wealth Advisor for <br className="hidden md:block" />
+        <section className="flex flex-col xl:flex-row relative px-8 py-10 mb-10 gap-12">
+            <div className="flex-1 flex flex-col justify-center space-y-8">
+                <h1 className="text-5xl md:text-[52px] font-normal leading-tight">
+                    The wealth advisor for <br className="hidden md:block" />
                     high-stakes financial <br className="hidden md:block" />
                     decisions
                 </h1>
 
-                <div className="flex flex-wrap items-center gap-6">
-                    <button className="bg-linear-to-b from-white to-stone-300 text-black w-44 h-11 rounded-[35px] text-[10px] font-medium tracking-wider outline outline-neutral-500">
+                <div className="flex flex-wrap items-center gap-10">
+                    <button className="bg-linear-to-b from-white to-stone-300 text-black w-44 h-11 rounded-[35px] text-[12px] font-semibold tracking-tighter outline cursor-pointer outline-neutral-500">
                         GET STARTED →
                     </button>
 
-                    <button className="outline-[0.5px] outline-neutral-500 text-white w-44 h-11 rounded-[35px] text-[10px] font-medium tracking-wider">
+                    <button className=" outline-neutral-500 text-white h-11 rounded-[35px] text-[12px] font-semibold tracking-tight cursor-pointer">
                         SEE WHY →
                     </button>
                 </div>
