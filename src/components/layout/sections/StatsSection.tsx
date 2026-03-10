4b49d6b4e385406c6cb7c4f2f34db676624ac2d9
Devexhhh <kumarsujal.codes@gmail.com>
2026-03-10 19:21:03 +0530

    fixed StatsSection

--- src/components/layout/sections/StatsSection.tsx
+++ src/components/layout/sections/StatsSection.tsx
@@ -3,10 +3,10 @@ import VerticalLine from "../../ui/VerticalLine";
 
 export default function StatsSection() {
     return (
-        <section className="flex flex-col xl:flex-row px-12 py-24 gap-12 relative">
+        <section className="flex flex-col xl:flex-row px-8 py-5 mb-10 gap-12 relative">
 
-            <div className="flex-1 flex items-center">
-                <p className="text-lg font-medium leading-relaxed max-w-xl text-stone-200">
+            <div className="flex-1 flex items-start">
+                <p className="text-lg font-light leading-relaxed max-w-xl text-stone-200">
                     Titan helps you navigate equity decisions, liquidity events,
                     and career transitions - with a dedicated human advisor and
                     a modern platform.
@@ -20,7 +20,7 @@ export default function StatsSection() {
             <div className="flex-1 flex flex-wrap md:flex-nowrap gap-12 items-center justify-between">
 
                 <div className="flex flex-col">
-                    <p className="text-stone-400 text-lg font-medium mb-2">
+                    <p className="text-stone-400 text-sm font-medium mb-2">
                         Assets Under Management
                     </p>
                     <p className="text-4xl font-medium tracking-[10.8px]">
@@ -33,7 +33,7 @@ export default function StatsSection() {
                 </div>
 
                 <div className="flex flex-col">
-                    <p className="text-stone-400 text-lg font-medium mb-2">
+                    <p className="text-stone-400 text-sm font-medium mb-2">
                         All-in Advisory Fee
                     </p>
                     <p className="text-4xl font-medium tracking-[7.2px]">
@@ -46,7 +46,7 @@ export default function StatsSection() {
                 </div>
 
                 <div className="flex flex-col">
-                    <p className="text-stone-400 text-lg font-medium mb-2">
+                    <p className="text-stone-400 text-sm font-medium mb-2">
                         Flat Fee
                     </p>
                     <p className="text-4xl font-medium">$0</p>
